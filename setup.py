c906ecfac3071fdef91fcf22177b1632e858a0c7
lucky <17835344407@163.com>
2025-04-20 23:18:51 +0800

    fix: add fa3

--- setup.py
+++ setup.py
@@ -284,7 +284,7 @@ def get_version() -> str:
 
 
 ext_modules.append(CMakeExtension(name="vllm_flash_attn._vllm_fa2_C"))
-# ext_modules.append(CMakeExtension(name="vllm_flash_attn._vllm_fa3_C"))
+ext_modules.append(CMakeExtension(name="vllm_flash_attn._vllm_fa3_C"))
 
 setup(
     name="vllm-flash-attn",
